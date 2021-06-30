2c22cb07b54200cc3cbbb962e1f7e451f32e1992
acigorgutierrezdc <ac.igor.gutierrez.dc@gmail.com>
2021-06-30 18:39:22 -0300

    DbContext classes.

---
+++ Curso/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoEFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=True;");
+        }
+    }
+}
